8ba015bd7026c754bf6c7e75fe7e7710bd6908fa
Hemanga619 <hemangabharali619@gmail.com>
2024-02-02 19:40:46 +0530

    Modified Empty Project
    Slots for everything

---
+++ Kriti2024/Assets/Scripts/TranslateMotion.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering.LookDev;
+using UnityEngine;
+
+public class TranslateMotion : MonoBehaviour
+{
+    Vector2 difference = Vector2.zero;
+    Camera cameras;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameras = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    
+    private void OnMouseDown() {
+        // Record the difference between the objects centre, and the clicked point on the camera plane.
+        difference = (Vector2)cameras.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+    }
+    
+    private void OnMouseDrag(){
+        transform.position = new Vector2(((Vector2)cameras.ScreenToWorldPoint(Input.mousePosition) - difference).x,transform.position.y);
+    }
+
+    
+}
